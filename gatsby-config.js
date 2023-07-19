d21128242f8d6be43e127ea3d5607cf47f90398e
Bimo Arsa <zetvies@gmail.com>
2023-07-19 16:17:10 +0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `svara-melodia-web`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
